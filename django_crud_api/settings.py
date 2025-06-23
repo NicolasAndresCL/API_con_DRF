11eeb7b78a97edf6ce34df512a1c36fad550fbba
Nicolas Cano <nicolas.cano.leal@gmail.com>
2025-06-23 03:52:04 -0400

    Migrated database from SQLite3 to MySQL and updated settings.py accordingly. Verified functionality of API and admin panel post-migration.

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -82,8 +82,15 @@ WSGI_APPLICATION = 'django_crud_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'api_drf_db',
+        'USER': 'root',
+        'PASSWORD': 'Kerubin7$',
+        'HOST': 'localhost',  # o la IP si es remoto
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
